465792eef2b4a3e00538fde47cd9ca132fbd6863
diegobarr <d1390xd.v@gmail.com>
2025-09-09 17:35:23 -0600

    Codigo terminado con algunas validaciones pendientes

--- menuInteractivo.py
+++ menuInteractivo.py
@@ -1,3 +1,5 @@
+digitos=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 def factorial(n):
     if n == 0 or n == 1:
         return 1
@@ -33,6 +35,11 @@ def invertir_texto(palabra):
     else:
         return invertir_texto(palabra[1:]) + palabra[0]
 
+def potencia(n,exp):
+    if exp==0:
+        return 1
+    else:
+        return n*potencia(n,exp-1)
 
 
 
@@ -43,36 +50,88 @@ while True:
     print("3. Calcular la secuencia de Fibonacci")
     print("4. Contador de letras")
     print("5. Invertir texto")
+    print("6. Calcular una potencia")
+    print("7. Salir")
 
     opcion=input("\nSeleccione una opción: ")
 
     match opcion:
         case "1":
             print("Calcualar el factorial de un numero")
-            numero = int(input("\nIngrese su número: "))
-            print(f"El factorial de {numero} es {factorial(numero)}\n")
+            try:
+                numero = int(input("\nIngrese su número: "))
+                if numero>=0:
+                    print(f"El factorial de {numero} es {factorial(numero)}\n")
+                else:
+                    print("El numero debe ser un entero positivo\n")
+
+            except ValueError:
+                print("El numero debe ser un entero positivo\n")
 
         case "2":
             print("Suma de numero naturales hasta el deseado")
-            numero=int(input("Ingrese su número natural: "))
-            print(f"La suma de todos los numeros naturales hasta {numero} es {suma_naturales(numero)}\n")
+
+            try:
+                numero=int(input("\nIngrese su número natural: "))
+
+                if numero>=0:
+                    print(f"La suma de todos los numeros naturales hasta {numero} es {suma_naturales(numero)}\n")
+                else:
+                    print("El numero debe ser un entero positivo\n")
+            except ValueError:
+                print("El numero debe ser un entero positivo\n")
 
         case "3":
             print("Calcular la secuencia de Fibonacci en la posición deseada")
-            posicion=int(input("Ingrese la posicion Fibonacci que desee conocer: "))
+            posicion=int(input("\nIngrese la posicion Fibonacci que desee conocer: "))
 
             print(f"El numero de Fibonacci en la posición {posicion} es {secuencia_Fibonacci(posicion)}\n")
 
         case "4":
             print("Contar letras dentro de una palabra")
 
-            palabra=input("Ingrese una palabra: ")
+            palabra=str(input("\nIngrese una palabra: "))
             letra=input("Ingrese una letra: ")
 
-            print(f"La letra '{letra}' se encuentra {contador_letras(palabra, letra)} veces dentro de la palabra {palabra}\n")
+            if palabra.strip()=="" or letra.strip()=="":
+                print("Ninguno de los campos debe quedar vacio\n")
+
+            elif len(letra) != 1:
+                print("Solo se debe ingresar una letra\n")
+
+            elif letra.isdigit():
+                print("Se debe ingresar una letra\n")
+
+            else:
+                numeros=False
+                for i in palabra:
+                    if i in digitos:
+                        numeros=True
+                        break
+
+                if numeros:
+                    print("La palabra no debe contener números\n")
+
+                else:
+                    print(f"La letra '{letra}' se encuentra {contador_letras(palabra, letra)} veces dentro de la palabra {palabra}\n")
 
         case "5":
             print("Invertir una cadena de texto")
-            texto=input("Ingrese una cadena de texto: ")
+
+            texto=input("\nIngrese una cadena de texto: ")
 
             print(f"El texto invertido es '{invertir_texto(texto)}'\n")
+
+        case "6":
+            print("Potencia de un número")
+
+            base=int(input("\nIngrese el número: "))
+            exponente=int(input("Ingrese el exponente: "))
+
+            print(f"La potencia es de {potencia(base,exponente)}\n")
+
+        case "7":
+            print("Saliendo del programa")
+
+        case _:
+            print("Opción no válida")
