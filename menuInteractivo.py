21f63c5e11026f57ff70d9441625d7936df51fd0
diegobarr <d1390xd.v@gmail.com>
2025-09-09 16:53:55 -0600

    Fibbonacci y cadenas de texto terminadas

--- menuInteractivo.py
+++ menuInteractivo.py
@@ -11,19 +11,38 @@ def suma_naturales(n):
         return n + suma_naturales(n-1)
 
 def secuencia_Fibonacci(n):
-    numeros = [0, 1]
-
-    if numeros[n-1]:
-        return numeros[n]
+    if n == 0:
+        return 0
+    if n==1:
+        return 1
     else:
-        numeros.append(numeros[n-1] + numeros[n-2])
-        return numeros[n-1] + numeros[n-2]
+        return secuencia_Fibonacci(n-1)+secuencia_Fibonacci(n-2)
+
+def contador_letras(palabra, letra):
+    if palabra=="":
+        return 0
+
+    if palabra[0]==letra:
+        return 1 + contador_letras(palabra[1:], letra)
+    else:
+        return contador_letras(palabra[1:], letra)
+
+def invertir_texto(palabra):
+    if palabra=="":
+        return ""
+    else:
+        return invertir_texto(palabra[1:]) + palabra[0]
+
+
+
 
 while True:
     print("___Bienvenido al menu de opciones___")
     print("1. Calcular el factorial de un numero")
     print("2. Calcular la suma de todos los numeros naturales hasta el deseado")
     print("3. Calcular la secuencia de Fibonacci")
+    print("4. Contador de letras")
+    print("5. Invertir texto")
 
     opcion=input("\nSeleccione una opción: ")
 
@@ -43,3 +62,17 @@ while True:
             posicion=int(input("Ingrese la posicion Fibonacci que desee conocer: "))
 
             print(f"El numero de Fibonacci en la posición {posicion} es {secuencia_Fibonacci(posicion)}\n")
+
+        case "4":
+            print("Contar letras dentro de una palabra")
+
+            palabra=input("Ingrese una palabra: ")
+            letra=input("Ingrese una letra: ")
+
+            print(f"La letra '{letra}' se encuentra {contador_letras(palabra, letra)} veces dentro de la palabra {palabra}\n")
+
+        case "5":
+            print("Invertir una cadena de texto")
+            texto=input("Ingrese una cadena de texto: ")
+
+            print(f"El texto invertido es '{invertir_texto(texto)}'\n")
